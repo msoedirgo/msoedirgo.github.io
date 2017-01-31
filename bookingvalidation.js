14d27474d302f07b784791b5be3505d019d7b6f0
maria <mariasoedirgo@gmail.com>
2017-01-31 23:09:57 +0700

    create bookingvalidation

--- bookingvalidation.js
+++ bookingvalidation.js
@@ -2,5 +2,43 @@ function returnformValidation() {
 var uorigin = document.validity.origin;
 var udestination = document.validity.destination;
 var udepartdate = document.validity.departdate;
-var ureturndate = document.validity.returndate;
-var 
+var unumberadult = document.validity.numberadult;
+
+if (cityselect(uorigin)) {
+if (cityselect(udestination)) {
+if (passenger(unumberadult)) {
+}
+}
+}  
+return false;
+}
+
+function cityselect(uorigin) {
+  if (uorigin.value == "Default") {
+    alert ('Select your city of origin from the list');
+    uorigin.focus();
+    return false;
+  }
+  else {
+    return true;
+  }
+  
+function cityselect(udestination) {
+  if (udestination.value == "Default") {
+    alert ('Select your city of destination from the list');
+    udestination.focus();
+    return false;
+  }
+  else {
+    return true;
+  }
+  
+function passenger(unumberadult) {
+  if (unumberadult == 0) {
+    alert ('Select number of adults');
+    unumberadult.focus();
+    return false;
+  }
+  else {
+    return true
+  }
