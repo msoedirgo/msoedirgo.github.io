ce9e84c1ce869525ddae72199bbb368fff880b62
maria <mariasoedirgo@gmail.com>
2017-01-31 18:18:45 +0700

    Create bookingvalidation.js

---
+++ bookingvalidation.js
@@ -0,0 +1,6 @@
+function returnformValidation() {
+var uorigin = document.validity.origin;
+var udestination = document.validity.destination;
+var udepartdate = document.validity.departdate;
+var ureturndate = document.validity.returndate;
+var 
